2a2c2fd937f9ee3175afed63d3e21e0905e5051c
creationPrimal <143657193+creationPrimal@users.noreply.github.com>
2024-01-11 07:12:06 -0800

    Add files via upload

--- assets/js/loader.js
+++ assets/js/loader.js
@@ -67,3 +67,31 @@ function copyAndShare() {
       alert('Failed to copy URL to clipboard');
     });
 }
+
+//implementing dark mode
+
+const darkmodeButton = document.getElementById('darkmode');
+const body = document.body;
+
+  function toggleDarkMode() {
+    body.classList.toggle('dark-mode');
+    const isDarkMode = body.classList.contains('dark-mode');
+    localStorage.setItem('isDarkMode', isDarkMode);
+  }
+
+  darkmodeButton.addEventListener('click', toggleDarkMode);
+
+  // Check if dark mode is stored in local storage on page load
+  window.addEventListener('load', () => {
+    const isDarkMode = JSON.parse(localStorage.getItem('isDarkMode'));
+    if (isDarkMode) {
+      body.classList.add('dark-mode');
+    }
+  });
+
+
+    
+    
+
+
+
