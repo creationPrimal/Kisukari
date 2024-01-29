b10a7e84199b03e282a8195e3129202e96e4479f
creationPrimal <143657193+creationPrimal@users.noreply.github.com>
2024-01-29 00:42:43 -0800

    Add files via upload

--- assets/js/sliders.js
+++ assets/js/sliders.js
@@ -287,4 +287,436 @@ const fourthsplide = new Splide( '#fourth-splide', {
     } );
     fourthsplide.mount()
 
+//fifth slider
+const fifthsplide = new Splide( '#fifth-splide', {
+  perPage: 4,
+  gap : '1.8rem',
+  drag : 'free',
+  perMove: 1,
+  padding: '0px 10px',
+  snap: true,
+  type: 'loop',
+  pagination: false,
+  autoplay: false,
+  arrows: true,
+  breakpoints: {
+    1199: {
+      perPage: 3,
+      gap : '10px',
+      padding: { left: 0, right: 50 },
+    },
+    991: {
+      perPage: 2,
+      padding: { left: 0, right: 100 },
+    },
+    764: {
+      perPage: 2,
+      padding: { left: 0, right: 0 },
+    },
+    575: {
+      perPage: 2,
+      padding: { left: 0, right: 80 },
+    },
+    515: {
+      perPage: 2,
+      padding: { left: 0, right: 40 },
+    },
+    480: {
+      perPage: 2,
+      padding: { left: 0, right: 5 },
+    },
+    450: {
+    perPage: 2,
+    padding: { left: 0, right: 80 },
+    
+    },
+    415: {
+      perPage: 2,
+      padding: { left: 0, right: 70 },
+      
+      },
+      390: {
+          perPage: 2,
+          padding: { left: 0, right: 20 },
+      },
+      355: {
+          perPage: 2,
+          padding: { left: 0, right: 0 },
+      },
+      335: {
+          perPage: 1,
+          padding: { left: 0, right: 130 },
+      },
+      305: {
+          perPage: 1,
+          padding: { left: 0, right: 100 },
+      },
+      275: {
+          perPage: 1,
+          padding: { left: 0, right: 60 },
+      },
+  },
+  } );
+  fifthsplide.mount()
+
+//sixth slider
+const sixthsplide = new Splide( '#sixth-splide', {
+  perPage: 4,
+  gap : '1.8rem',
+  drag : 'free',
+  perMove: 1,
+  padding: '0px 10px',
+  snap: true,
+  type: 'loop',
+  pagination: false,
+  autoplay: false,
+  arrows: true,
+  breakpoints: {
+    1199: {
+      perPage: 3,
+      gap : '10px',
+      padding: { left: 0, right: 50 },
+    },
+    991: {
+      perPage: 2,
+      padding: { left: 0, right: 100 },
+    },
+    764: {
+      perPage: 2,
+      padding: { left: 0, right: 0 },
+    },
+    575: {
+      perPage: 2,
+      padding: { left: 0, right: 80 },
+    },
+    515: {
+      perPage: 2,
+      padding: { left: 0, right: 40 },
+    },
+     480: {
+      perPage: 2,
+      padding: { left: 0, right: 5 },
+    },
+    450: {
+    perPage: 2,
+    padding: { left: 0, right: 80 },
+    
+    },
+    415: {
+      perPage: 2,
+      padding: { left: 0, right: 70 },
+      
+      },
+      390: {
+          perPage: 2,
+          padding: { left: 0, right: 20 },
+      },
+      355: {
+          perPage: 2,
+          padding: { left: 0, right: 0 },
+      },
+      335: {
+          perPage: 1,
+          padding: { left: 0, right: 130 },
+      },
+      305: {
+          perPage: 1,
+          padding: { left: 0, right: 100 },
+      },
+      275: {
+          perPage: 1,
+          padding: { left: 0, right: 60 },
+      },
+  },
+  } );
+  sixthsplide.mount()
+
+//seventh slider
+const seventhsplide = new Splide( '#seventh-splide', {
+  perPage: 4,
+  gap : '1.8rem',
+  drag : 'free',
+  perMove: 1,
+  padding: '0px 10px',
+  snap: true,
+  type: 'loop',
+  pagination: false,
+  autoplay: false,
+  arrows: true,
+  breakpoints: {
+    1199: {
+      perPage: 3,
+      gap : '10px',
+      padding: { left: 0, right: 50 },
+    },
+    991: {
+      perPage: 2,
+      padding: { left: 0, right: 100 },
+    },
+    764: {
+      perPage: 2,
+      padding: { left: 0, right: 0 },
+    },
+    575: {
+      perPage: 2,
+      padding: { left: 0, right: 80 },
+    },
+    515: {
+      perPage: 2,
+      padding: { left: 0, right: 40 },
+    },
+     480: {
+      perPage: 2,
+      padding: { left: 0, right: 5 },
+    },
+    450: {
+    perPage: 2,
+    padding: { left: 0, right: 80 },
+    
+    },
+    415: {
+      perPage: 2,
+      padding: { left: 0, right: 70 },
+      
+      },
+      390: {
+          perPage: 2,
+          padding: { left: 0, right: 20 },
+      },
+      355: {
+          perPage: 2,
+          padding: { left: 0, right: 0 },
+      },
+      335: {
+          perPage: 1,
+          padding: { left: 0, right: 130 },
+      },
+      305: {
+          perPage: 1,
+          padding: { left: 0, right: 100 },
+      },
+      275: {
+          perPage: 1,
+          padding: { left: 0, right: 60 },
+      },
+  },
+  } );
+  seventhsplide.mount()
+
+//eight slider
+const eighthsplide = new Splide( '#eighth-splide', {
+  perPage: 4,
+  gap : '1.8rem',
+  drag : 'free',
+  perMove: 1,
+  padding: '0px 10px',
+  snap: true,
+  type: 'loop',
+  pagination: false,
+  autoplay: false,
+  arrows: true,
+  breakpoints: {
+    1199: {
+      perPage: 3,
+      gap : '10px',
+      padding: { left: 0, right: 50 },
+    },
+    991: {
+      perPage: 2,
+      padding: { left: 0, right: 100 },
+    },
+    764: {
+      perPage: 2,
+      padding: { left: 0, right: 0 },
+    },
+    575: {
+      perPage: 2,
+      padding: { left: 0, right: 80 },
+    },
+    515: {
+      perPage: 2,
+      padding: { left: 0, right: 40 },
+    },
+     480: {
+      perPage: 2,
+      padding: { left: 0, right: 5 },
+    },
+    450: {
+    perPage: 2,
+    padding: { left: 0, right: 80 },
+    
+    },
+    415: {
+      perPage: 2,
+      padding: { left: 0, right: 70 },
+      
+      },
+      390: {
+          perPage: 2,
+          padding: { left: 0, right: 20 },
+      },
+      355: {
+          perPage: 2,
+          padding: { left: 0, right: 0 },
+      },
+      335: {
+          perPage: 1,
+          padding: { left: 0, right: 130 },
+      },
+      305: {
+          perPage: 1,
+          padding: { left: 0, right: 100 },
+      },
+      275: {
+          perPage: 1,
+          padding: { left: 0, right: 60 },
+      },
+  },
+  } );
+  eighthsplide.mount()
+
+//nineth slider
+const ninethsplide = new Splide( '#nineth-splide', {
+  perPage: 4,
+  gap : '1.8rem',
+  drag : 'free',
+  perMove: 1,
+  padding: '0px 10px',
+  snap: true,
+  type: 'loop',
+  pagination: false,
+  autoplay: false,
+  arrows: true,
+  breakpoints: {
+    1199: {
+      perPage: 3,
+      gap : '10px',
+      padding: { left: 0, right: 50 },
+    },
+    991: {
+      perPage: 2,
+      padding: { left: 0, right: 100 },
+    },
+    764: {
+      perPage: 2,
+      padding: { left: 0, right: 0 },
+    },
+    575: {
+      perPage: 2,
+      padding: { left: 0, right: 80 },
+    },
+    515: {
+      perPage: 2,
+      padding: { left: 0, right: 40 },
+    },
+     480: {
+      perPage: 2,
+      padding: { left: 0, right: 5 },
+    },
+    450: {
+    perPage: 2,
+    padding: { left: 0, right: 80 },
+    
+    },
+    415: {
+      perPage: 2,
+      padding: { left: 0, right: 70 },
+      
+      },
+      390: {
+          perPage: 2,
+          padding: { left: 0, right: 20 },
+      },
+      355: {
+          perPage: 2,
+          padding: { left: 0, right: 0 },
+      },
+      335: {
+          perPage: 1,
+          padding: { left: 0, right: 130 },
+      },
+      305: {
+          perPage: 1,
+          padding: { left: 0, right: 100 },
+      },
+      275: {
+          perPage: 1,
+          padding: { left: 0, right: 60 },
+      },
+  },
+  } );
+  ninethsplide.mount()
+
+//tenth slider
+const tenthsplide = new Splide( '#tenth-splide', {
+  perPage: 4,
+  gap : '1.8rem',
+  drag : 'free',
+  perMove: 1,
+  padding: '0px 10px',
+  snap: true,
+  type: 'loop',
+  pagination: false,
+  autoplay: false,
+  arrows: true,
+  breakpoints: {
+    1199: {
+      perPage: 3,
+      gap : '10px',
+      padding: { left: 0, right: 50 },
+    },
+    991: {
+      perPage: 2,
+      padding: { left: 0, right: 100 },
+    },
+    764: {
+      perPage: 2,
+      padding: { left: 0, right: 0 },
+    },
+    575: {
+      perPage: 2,
+      padding: { left: 0, right: 80 },
+    },
+    515: {
+      perPage: 2,
+      padding: { left: 0, right: 40 },
+    },
+     480: {
+      perPage: 2,
+      padding: { left: 0, right: 5 },
+    },
+    450: {
+    perPage: 2,
+    padding: { left: 0, right: 80 },
+    
+    },
+    415: {
+      perPage: 2,
+      padding: { left: 0, right: 70 },
+      
+      },
+      390: {
+          perPage: 2,
+          padding: { left: 0, right: 20 },
+      },
+      355: {
+          perPage: 2,
+          padding: { left: 0, right: 0 },
+      },
+      335: {
+          perPage: 1,
+          padding: { left: 0, right: 130 },
+      },
+      305: {
+          perPage: 1,
+          padding: { left: 0, right: 100 },
+      },
+      275: {
+          perPage: 1,
+          padding: { left: 0, right: 60 },
+      },
+  },
+  } );
+  tenthsplide.mount()
+
 
