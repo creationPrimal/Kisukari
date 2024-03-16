f61c2ec8982b4268688dbbcd1f6aa7d96c3d5778
creationPrimal <143657193+creationPrimal@users.noreply.github.com>
2024-03-16 02:18:18 -0700

    Add files via upload

--- assets/js/calc.js
+++ assets/js/calc.js
@@ -7,64 +7,64 @@
    const button2 = document.getElementById('button2');
    
    button1.addEventListener("click", ()=>{
-       button1.style.backgroundColor = '#d9232d';
+       button1.style.backgroundColor = 'rgb(0, 148, 0)';
        button1.style.color = 'white';
        button2.style.color = 'black';
-       button2.style.backgroundColor = 'rgba(255, 255, 255, 0.555)'; 
+       button2.style.backgroundColor = 'white'; 
    })
    button2.addEventListener("click", ()=>{
-       button2.style.backgroundColor = '#d9232d';
+       button2.style.backgroundColor = 'rgb(0, 148, 0)';
        button2.style.color = 'white';
        button1.style.color = 'black';
-       button1.style.backgroundColor = 'rgba(255, 255, 255, 0.555)';
+       button1.style.backgroundColor = 'white';
    })
 
    const button3 = document.getElementById('button3');
    const button4 = document.getElementById('button4');
    
    button3.addEventListener("click", ()=>{
-       button3.style.backgroundColor = '#d9232d';
+       button3.style.backgroundColor = 'rgb(0, 148, 0)';
        button3.style.color = 'white';
        button4.style.color = 'black';
-       button4.style.backgroundColor = 'rgba(255, 255, 255, 0.555)'; 
+       button4.style.backgroundColor = 'white'; 
    })
    button4.addEventListener("click", ()=>{
-       button4.style.backgroundColor = '#d9232d';
+       button4.style.backgroundColor = 'rgb(0, 148, 0)';
        button4.style.color = 'white';
        button3.style.color = 'black';
-       button3.style.backgroundColor = 'rgba(255, 255, 255, 0.555)';
+       button3.style.backgroundColor = 'white';
    })
 
    const button5 = document.getElementById('button5');
    const button6 = document.getElementById('button6');
    
    button5.addEventListener("click", ()=>{
-       button5.style.backgroundColor = '#d9232d';
+       button5.style.backgroundColor = 'rgb(0, 148, 0)';
        button5.style.color = 'white';
        button6.style.color = 'black';
-       button6.style.backgroundColor = 'rgba(255, 255, 255, 0.555)'; 
+       button6.style.backgroundColor = 'white'; 
    })
    button6.addEventListener("click", ()=>{
-       button6.style.backgroundColor = '#d9232d';
+       button6.style.backgroundColor = 'rgb(0, 148, 0)';
        button6.style.color = 'white';
        button5.style.color = 'black';
-       button5.style.backgroundColor = 'rgba(255, 255, 255, 0.555)';
+       button5.style.backgroundColor = 'white';
    })
 
    const button7 = document.getElementById('button7');
    const button8 = document.getElementById('button8');
    
    button7.addEventListener("click", ()=>{
-       button7.style.backgroundColor = '#d9232d';
+       button7.style.backgroundColor = 'rgb(0, 148, 0)';
        button7.style.color = 'white';
        button8.style.color = 'black';
-       button8.style.backgroundColor = 'rgba(255, 255, 255, 0.555)'; 
+       button8.style.backgroundColor = 'white'; 
    })
    button8.addEventListener("click", ()=>{
-       button8.style.backgroundColor = '#d9232d';
+       button8.style.backgroundColor = 'rgb(0, 148, 0)';
        button8.style.color = 'white';
        button7.style.color = 'black';
-       button7.style.backgroundColor = 'rgba(255, 255, 255, 0.555)';
+       button7.style.backgroundColor = 'white';
    })
 
 
